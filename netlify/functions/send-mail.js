b07e2d720bc484d3476af085718007ad13d93251
venom32177042 <92434484+venom32177042@users.noreply.github.com>
2025-12-17 10:06:37 +0300

    Update send-mail.js

--- netlify/functions/send-mail.js
+++ netlify/functions/send-mail.js
@@ -3,22 +3,40 @@ import { Resend } from "resend";
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function handler(event) {
+  // السماح فقط بـ POST
+  if (event.httpMethod !== "POST") {
+    return {
+      statusCode: 405,
+      body: "Method Not Allowed"
+    };
+  }
+
   try {
+    const { name, email, message } = JSON.parse(event.body || "{}" );
+
+    if (!name || !email || !message) {
+      return {
+        statusCode: 400,
+        body: "Missing fields"
+      };
+    }
+
     await resend.emails.send({
       from: "onboarding@resend.dev",
-      to: "king32177042@gmail.com",
-      subject: "Resend + Netlify Test",
-      text: "Backend يعمل بنجاح"
+      to: "YOUR_EMAIL@gmail.com",
+      subject: "New Message",
+      text: `Name: ${name}\nEmail: ${email}\n\n${message}`
     });
 
     return {
       statusCode: 200,
       body: JSON.stringify({ success: true })
     };
+
   } catch (error) {
     return {
       statusCode: 500,
       body: error.message
     };
   }
-        }
+}
